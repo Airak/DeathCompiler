f9e57b57fa07c593fcc7a82fd5b438edfdcdb7ae
Airak <matheus.mmag@gmail.com>
2017-04-20 18:46:38 -0300

    Classes Num, Tag, Token e Word.

---
+++ JavaApplication3/src/main/Tag.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Tag {
+    public final static int
+            //Palavras reservadas
+            INIT = 257,
+            STOP = 258,
+            IS = 259,
+            INTEGER = 260,
+            STRING = 261,
+            IF = 262,
+            BEGIN = 263,
+            END = 264,
+            ELSE = 265,
+            DO = 266,
+            WHILE = 267,
+            READ = 268,
+            WRITE = 269,
+            NOT = 270,
+            OR = 271,
+            AND = 272,
+            TRUE = 273,
+            FALSE = 274,
+            //Operadores
+            EQ=290,
+            GE=291,
+            LE=292,
+            NE=293,            
+            //Outros tokens
+            NUM = 300,
+            ID = 301;   
+}
